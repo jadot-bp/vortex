882dc63a8179895da2d1d5c7ab98fd37f9b7ee25
Ben Page <benjaminpage.acer@gmail.com>
2023-02-06 10:48:37 +0000

    Removed general gauge transform parameter. Created new function to calculate conjugate momentum.

--- gluon_utils.py
+++ gluon_utils.py
@@ -2,16 +2,18 @@ import lyncs_io as io
 import numpy as np
 
 class lattice:                                                             
-    def __init__(self, lattice, params, gauge_transform = None, is_conjugate=False):                                        
+    def __init__(self, lattice, params, is_conjugate=False):                                        
         self.lattice = lattice                                                  
         self.Nt, self.Ns, self.Nd, self.Nc = params        
-        self.gauge_transform = gauge_transform
         
-        self.shape = (self.Nt, self.Ns, self.Ns, self.Ns)
+        self.shape = tuple([self.Nt] + [self.Ns]*(self.Nd-1))
+        self.V = np.prod(self.shape)
         self._is_conjugate = is_conjugate
+    
     
     def __repr__(self):
         return "lattice(lattice, params=(Nt,Ns,Nd,Nc), gauge_transform [optional])"
+    
     
     def transform(self):
         """Performs the Fourier transform over the lattice."""
@@ -19,34 +21,36 @@ class lattice:
         self.lattice = np.fft.fftn(self.lattice,axes=(0,1,2,3))
         self._is_conjugate = not self._is_conjugate
         
+        
     def inverse_transform(self):
         """Performs the inverse Fourier transform over the lattice."""
                
         self.lattice = np.fft.ifftn(self.lattice,axes=(0,1,2,3))
         self._is_conjugate = not self._is_conjugate
     
-    def get_lattice(self):                                                       
-        return self.lattice                                                     
-                                                                                
-    def get_link(self, coord, mu, apply_transform = False):       
-        pos = *coord, mu
-        
-        if apply_transform == True:
-            if not isinstance(self.gauge_transform,np.ndarray):
-                raise Exception("Gauge transformation field not specified.")
-                
-            gx = self.gauge_transform[tuple(coord)]
-
-            coord_mu = list(coord)
-            coord_mu[mu] += 1
-            coord_mu[mu] %= self.shape[mu]
-            gxmu = self.gauge_transform[tuple(coord_mu)]
-            return np.matmul(gx,np.matmul(self.lattice[pos],np.conj(gxmu).T))
-        else:
-            return self.lattice[pos]
     
-    def get_plaquette(self, coord, plane,apply_transform = False):                                      
-        """ Calculates plaquette at coordinate in plane direction.              
+    def get_lattice(self):
+        """Fetches the stored lattice."""
+        return self.lattice                                                     
+                   
+        
+    def get_link(self, coord, mu, ):
+        """Fetches the link field in the mu-direction at coord."""
+        
+        coord = np.asarray(coord)
+        
+        if np.any(coord < 0) and not self._is_conjugate:
+            raise IndexError("Negative coordinates not supported for non-conjugated lattices.")
+        if np.any(2*coord/np.asarray(self.shape) > 1) and self._is_conjugate:
+            raise IndexError("Coordinates greater in magnitude than half the lattice extent invalid for conjugated lattices.")
+        
+        pos = np.array(([*coord, mu]))
+        
+        return self.lattice[tuple(pos)]
+    
+    
+    def get_plaquette(self, coord, plane):                                      
+        """ Calculates plaquette at coord in plane direction.              
                                                                                 
         coord : coordinate in format (t,x,y,z)                                  
         plane : direction plaquette plane in format (mu, nu) (e.g. (1,2) for (x,y) plane)
@@ -71,50 +75,62 @@ class lattice:
         P = 0                                                                   
                                                                                 
         # get link in mu direction at coord                                     
-        U_mu = self.get_link(coord,mu,apply_transform)                                          
+        U_mu = self.get_link(coord,mu)                                          
                                                                                 
         P = U_mu                                                                
                                                                                 
         # get link in nu direction at coord + mu                                
         next_coord = np.asarray(coord) + mu_step                                
         next_coord[mu] %= mu_extent                                             
-        U_nu = self.get_link(next_coord,nu,apply_transform)                                     
+        U_nu = self.get_link(next_coord,nu)                                     
                                                                                 
         P = np.matmul(P,U_nu)                                                   
                                                                                 
        # get daggered link in mu direction at coord + nu                           
         next_coord = np.asarray(coord) + nu_step   
         next_coord[nu] %= nu_extent                                             
-        U_mu_dag = np.conj(self.get_link(next_coord,mu,apply_transform)).T                      
+        U_mu_dag = np.conj(self.get_link(next_coord,mu)).T                      
                                                                                 
         P = np.matmul(P,U_mu_dag)                                               
                                                                                 
         # get daggered link in nu direction at coord                               
         next_coord = np.asarray(coord)                                          
-        U_mu_nu_dag = np.conj(self.get_link(next_coord,nu,apply_transform)).T                   
+        U_mu_nu_dag = np.conj(self.get_link(next_coord,nu)).T                   
                                                                                 
         P = np.matmul(P,U_mu_nu_dag)                                            
                                                                                 
         return P
     
     def evaluate_average_plaquette():
+        ## TO DO
         return None
     
-    def get_A(self, coord, mu):
+    def get_qhat(self,coord,mu,a=1):
+        """Calculates q^_\mu corresponding to coord."""
+        
+        coord = np.asarray(coord)
+        
+        return (2*np.pi/a)*(coord[mu]/self.shape[mu])
+    
+    def get_A(self, coord, mu, a=1):
+        
+        coord = np.asarray(coord)
         
         if not self._is_conjugate:
             # Fetch A = (U-U^\dag)_traceless/2i
-            U = self.get_link(coord)
+            U = self.get_link(coord,mu)
             
             return (U-np.conj(U.T))/2j - np.trace(U-np.conj(U.T))/(2j*self.Nc)
         
         else:
         
             U = self.get_link(coord,mu)
-            U_neg = self.get_link(-np.asarray(coord),mu)
+            U_neg = self.get_link(-coord,mu)
             B = U - np.conj(U_neg.T)
         
-            A = B - np.trace(B)/3
+            return np.exp(-0.5j*a*self.get_qhat(coord, mu))/(2j*a) * (B - np.trace(B)/self.Nc)
+            
+            
         
 def gell_mann(number=None):
     
