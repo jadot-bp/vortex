e607c62ef27ea9cc1536aa00072ad1d713308a81
Ben Page <benjaminpage.acer@gmail.com>
2023-02-26 17:16:36 +0000

    Header file for gluon_utils.

---
+++ gluon_utils.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <complex.h>
+
+#ifndef __gluon_utils_h__
+#define __gluon_utils_h__
+
+double complex gauge_transform(double complex U[], double complex gauge[], int Nt, int Ns, int Nd, int Nc);
+
+#endif
+
+
