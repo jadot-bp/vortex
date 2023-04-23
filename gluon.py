86b2adf12a914422bb17a31dc3a9ec031c33f47e
Ben Page <benjaminpage.acer@gmail.com>
2023-04-23 13:21:20 +0100

    Updated to include search of available transforms. Updated according to new Fourier transform definition.

--- gluon.py
+++ gluon.py
@@ -8,6 +8,7 @@ import lyncs_io as io
 import numpy as np
 import time
 import itertools
+import os
 
 import matplotlib.pyplot as plt
 
@@ -40,10 +41,19 @@ def unique_permute(coord):
 def spatial(Nt,nc,t0,check_divA=False):   
     D_results = []
 
-    for n in np.arange(1,nc+1):
+    available_transforms = []
+    
+    for file in os.listdir(f"../gauge_confs/transforms/{Nt}x32"):
+        if file.endswith(".gauge.lime"):
+            base_name = file.rstrip(".gauge.lime")
+            available_transforms.append(base_name)
+            
+    selection = np.random.choice(available_transforms,size=nc,replace=False)
+    
+    for n in range(nc):
         #input_file = "Gen2l_64x32n1.lime"
-        input_file = f"../gauge_confs/{Nt}x32/Gen2l_{Nt}x32n{n}"
-        gauge_file = f"../gauge_confs/transforms/{Nt}x32/Gen2l_{Nt}x32n{n}.gauge.lime"
+        input_file = f"../gauge_confs/samples/{Nt}x32/{selection[n]}"
+        gauge_file = f"../gauge_confs/transforms/{Nt}x32/{selection[n]}.gauge.lime"
 
         data = io.load(input_file, format="openqcd")
         gauge = io.load(gauge_file, format="lime")
@@ -58,7 +68,7 @@ def spatial(Nt,nc,t0,check_divA=False):
             print("div.A:",gf.evaluate_divA())
         
         # Transform to Fourier space
-        gf.transform(axes=(1,2,3))
+        gf.transform(axes=(0,1,2,3))
 
         results = []
         q = []
@@ -66,42 +76,38 @@ def spatial(Nt,nc,t0,check_divA=False):
         # Loop over t,qx,qy,qz
         start = time.time()
         for t in [t0]:
-            for qx in range(gf.shape[1]//2):
-                for qy in range(qx,gf.shape[2]//2):
-                    for qz in range(qy,gf.shape[3]//2):
+            for qx in range(gf.shape[1]//4):
+                for qy in range(qx,gf.shape[2]//4):
+                    for qz in range(qy,gf.shape[3]//4):
                         D_sum = 0
                         
                         z3_coords = unique_permute([qx,qy,qz])
                         
                         for coord in z3_coords:
-                            for mu in [1,2,3]:
+                            for mu in [0,1,2,3]:
 
                                 # Decompose A in terms of Gell-Mann components
                                 A = gu.decompose_su3(gf.get_A((t,coord[0],coord[1],coord[2]),mu))
-                                A_neg = gu.decompose_su3(gf.get_A((t,-coord[0],-coord[1],-coord[2]),mu))
+                                A_neg = gu.decompose_su3(gf.get_A((-t,-coord[0],-coord[1],-coord[2]),mu))
 
                                 D_sum += np.dot(A,A_neg)
 
                         #results.append([[gf.get_qhat((t,qx,qy,qz),mu) for mu in [0,1,2,3]],D_sum])
                         q.append([t,qx,qy,qz])
-                        results.append([[t,qx,qy,qz],D_sum/len(z3_coords)])
+                        results.append(D_sum/len(z3_coords))
         end = time.time()
 
         results_cleaned = []
 
-        for result in results:
-            if np.all(result[0] == 0):
-                results_cleaned.append([0,result[1]*2/((gf.Nc**2-1)*gf.Nd*gf.V)])
+        for pos,result in enumerate(results):
+            if np.all(np.asarray(q)[pos] == 0):
+                results_cleaned.append(result*2/((gf.Nc**2-1)*gf.Nd*gf.V))
             else:
                 #results_cleaned.append([np.linalg.norm(q_improved(result[0])),result[1]*2/((gf.Nc**2-1)*(gf.Nd-1)*gf.V)])
-                results_cleaned.append([np.linalg.norm(result[0]),result[1]*2/((gf.Nc**2-1)*(gf.Nd-1)*gf.V)])
+                results_cleaned.append(result*2/((gf.Nc**2-1)*(gf.Nd-1)*gf.V))
 
         results_cleaned = np.asarray(results_cleaned)
-
-        if n==1:
-            D_results.append(results_cleaned[:,0])
-        D_results.append(results_cleaned[:,1])
-        
+        D_results.append(results_cleaned)       
         print(f"{input_file} done.")
 
     return q, D_results
