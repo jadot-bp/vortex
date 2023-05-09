3bfbdcd6c505efcc8f39b1b14cbf0efbd997b378
Ben Page <benjaminpage.acer@gmail.com>
2023-05-09 17:29:24 +0100

    Changed header file to reflect changes in gprop.c

--- gprop.h
+++ gprop.h
@@ -4,9 +4,7 @@
 #ifndef __gprop_h__
 #define __gprop_h__
 
-double complex gluon_prop(struct gluon_field gf, int mu, int nu, int a, int b, int t, int y[]);
-void calc_scalar_D(struct gluon_field gf, int t, double complex D[]);
-void pragma_test();
+void calc_mom_space_scalar_D(struct gluon_field gf, double complex D[]);
 
 #endif
 
