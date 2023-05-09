af9bc478ae49a8c69198321569d963e3e6ee9935
Ben Page <benjaminpage.acer@gmail.com>
2023-05-09 17:29:01 +0100

    Removed unnecessary declarations. Removed unused position space functions. Definedsupporting functions for momentum space propagator calculation. Created momentum space propagator function.

--- gprop.c
+++ gprop.c
@@ -4,10 +4,10 @@
 #include <omp.h>
 
 #define DIM 7
-#define MU_START 0
-#define NUM_THREADS 5
 
-int _calc_idx(int pos[], int shape[], int Nd){
+#define PI 3.14159265358979323846
+
+int _calc_idx(int pos[], int shape[], int dim){
     // Calculate the row-major address in  memory at position pos for given shape.
     //
     // Args:
@@ -18,10 +18,10 @@ int _calc_idx(int pos[], int shape[], int Nd){
         
     int idx = 0;
     
-    for(int k=0; k < Nd; k++){
+    for(int k=0; k < dim; k++){
         int prod_dim = 1;
         
-        for(int l=k+1; l < Nd; l++){
+        for(int l=k+1; l < dim; l++){
             prod_dim *= shape[l];
         }
         idx += prod_dim*pos[k];
@@ -29,7 +29,8 @@ int _calc_idx(int pos[], int shape[], int Nd){
     return idx;
 }
 
-int *decompose_su3(double complex M[3][3], double complex components[8]){
+void decompose_su3(double complex M[3][3], double complex components[8]){
+    // Decompose the matrix M[][] into the SU(3) Gell-Mann components, returned in components[]
     
     components[0] = creal(M[0][1]) + creal(M[1][0]);
     components[1] = -cimag(M[0][1]) + cimag(M[1][0]);
@@ -39,8 +40,39 @@ int *decompose_su3(double complex M[3][3], double complex components[8]){
     components[5] = creal(M[1][2]) + creal(M[2][1]);
     components[6] = -cimag(M[1][2]) + cimag(M[2][1]);
     components[7] = (creal(M[0][0]) + creal(M[1][1]) -2.0*creal(M[2][2]))/sqrt(3.0);
+}
+
+void trless_conj_subtract(double complex A[3][3], double complex B[3][3], double complex M[3][3]){
+    // Calculate the traceless conjugate subtraction of A[][] and B[][], returned in M[][]
+    //
+    // M = (A-B) - Tr(A-B)/Dim(A)
     
-    return 0;
+    double complex trace = 0;
+    
+    for(int i=0; i<3; i++){
+        trace += A[i][i] - conj(B[i][i]);
+    }
+    
+    trace /= 3;
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            M[i][j] = A[i][j] - conj(B[j][i]);
+        }
+    }
+    for(int i=0; i<3; i++){
+        M[i][i] -= trace;
+    }
+}
+
+void scalar_matmul(double complex scalar, double complex A[3][3], double complex M[3][3]){
+    // Multiply a matrix A[][] by a scalar, returned in M[][]
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            M[i][j] = scalar*A[i][j];
+        }
+    }
 }
 
 struct gluon_field {
@@ -49,104 +81,90 @@ struct gluon_field {
     int Ns;
     int Nd;
     int Nc;
-    double complex *A;
+    double complex *U;
 };
 
-double complex pos_space_gluon_prop(struct gluon_field *gf, int mu, int nu, int a, int b, int t, int y[4]){
-    // Calculate the position space gluon propagator D^ab_\mu\nu = <A^a_\mu(x)A^b_\nu(x+y)>/V
-    
-    int Nt = gf -> Nt;
-    int Ns = gf -> Ns;
-    int Nd = gf -> Nd;
-    int Nc = gf -> Nc;
-    
-    int A_shape[DIM] = {Nt, Ns, Ns, Ns, Nd, Nc, Nc};
-     
-    double complex runsum = 0; // Collect running two-point sum
-    
-    #pragma omp parallel reduction(+: runsum) num_threads(NUM_THREADS)
-    {
-        #pragma omp for
-        for(int i=0; i < Ns; i++){
-            for(int j=0; j<Ns; j++){
-                for(int k=0; k<Ns; k++){
-                    double complex A_mu[Nc][Nc];
-                    double complex A_nu[Nc][Nc];
-
-                    // Initialise position at beginning of SU(N) matrix
-                    int A_mu_pos[DIM] = {t,i,j,k,mu,0,0};
-                    int A_nu_pos[DIM] = {t,i,j,k,nu,0,0};
-
-                     // Translate A_\nu(x) -> A_\nu(x+y)
-                    for(int yi=0; yi<Nd; yi++){
-                        A_nu_pos[yi] += y[yi];
-                        A_nu_pos[yi] %= A_shape[yi]; // Enforce periodicity
-                    }
-
-                    // Calculate address in memory for A position
-                    int A_mu_idx = _calc_idx(A_mu_pos, A_shape, DIM);
-                    int A_nu_idx = _calc_idx(A_nu_pos, A_shape, DIM);
-
-                    // Construct gluon field matrix
-                    for(int c1=0; c1<Nc; c1++){
-                        for(int c2=0; c2<Nc; c2++){            
-                            A_mu[c1][c2] = gf->A[A_mu_idx];
-                            A_nu[c1][c2] = gf->A[A_nu_idx];
-
-                            // Increment address
-                            A_mu_idx += 1;
-                            A_nu_idx += 1;
-                        }
-                    }
-
-                    // Perform color decomposition
-                    double complex A_mu_components[8];
-                    double complex A_nu_components[8];
-
-                    decompose_su3(A_mu,A_mu_components);
-                    decompose_su3(A_nu,A_nu_components);
-
-                    // Update running two-point sum
-                    runsum += A_mu_components[a]*A_nu_components[b];
-                }
-            }
-        }
-    }
-    
-    return runsum/(Nt*Ns*Ns*Ns);
-}
-
-void calc_pos_space_scalar_D(struct gluon_field *gf, int t, double complex D[]){
+void calc_mom_space_scalarD(struct gluon_field *gf, double complex D[]){
     // Calculate the scalar propagator D in position space
     
     const int Nt = gf -> Nt;
     const int Ns = gf -> Ns;
     const int Nd = gf -> Nd;
     const int Nc = gf -> Nc;
-    
-    int D_shape[3] = {Ns, Ns, Ns};
-    
-    
-    {
-        for(int i=0; i<32; i++){
-            for(int j=0; j<32; j++){
-                for(int k=0; k<32; k++){
-                    int y[4] = {t,i,j,k}; 
-                    int y_pos[3] = {i,j,k}; // Position of y in D
 
-                    int d_pos = _calc_idx(y_pos, D_shape, Nd-1);
+    int U_shape[DIM] = {Nt, Ns, Ns, Ns, Nd, Nc, Nc};
+ 
+    int counter = 0;
 
-                    // Flush D
-                    D[d_pos] = 0;
+    for(int t=0; t<Nt/2; t++){
+        for(int qx=0; qx<Ns/2; qx++){
+            for(int qy=0; qy<Ns/2; qy++){
+                for(int qz=0; qz<Ns/2; qz++){
+                    
+                    D[counter] = 0.0;
 
-                    // Loop over Lorentz indices
-                    for(int mu=MU_START; mu<Nd; mu++){
-                        for(int a=0; a<Nc*Nc-1; a++){    
-                            D[d_pos] += gluon_prop(gf, mu, mu, a, a, t, y);
+                    for(int mu=1; mu<Nd; mu++){
+                        double complex U_pos[Nc][Nc];
+                        double complex U_neg[Nc][Nc];
+
+                        // Initialise position at beginning of SU(N) matrix
+                        int U_pos_coord[DIM] = {t,qx,qy,qz,mu,0,0};
+                        int U_neg_coord[DIM] = {(Nt-t)%Nt,(Ns-qx)%Ns,(Ns-qy)%Ns,(Ns-qz)%Ns,mu,0,0};
+
+                        // Calculate address in memory for A position
+                        int U_pos_idx = _calc_idx(U_pos_coord, U_shape, DIM);
+                        int U_neg_idx = _calc_idx(U_neg_coord, U_shape, DIM);
+                        
+                        // Construct link field matrix
+                        
+                        for(int c1=0; c1<Nc; c1++){
+                            for(int c2=0; c2<Nc; c2++){            
+                                U_pos[c1][c2] = gf->U[U_pos_idx];
+                                U_neg[c1][c2] = gf->U[U_neg_idx];
+
+                                // Increment address
+                                U_pos_idx += 1;
+                                U_neg_idx += 1;
+                            }
                         }
-                    }
-                }
-            }
-        }
-    }
-}
+                        
+                        // Calculate gluon field matrix
+                        
+                        double complex A[Nc][Nc];
+                        double complex A_neg[Nc][Nc];
+                        
+                        trless_conj_subtract(U_pos,U_neg,A);
+                        trless_conj_subtract(U_neg,U_pos,A_neg);
+ 
+                        scalar_matmul(cexp(-PI*I*U_pos_coord[mu]/Ns)/(2*I), A, A);
+                        scalar_matmul(cexp(PI*I*U_pos_coord[mu]/Ns)/(2*I), A_neg, A_neg);
+
+                        // Perform color decomposition
+                        double complex A_components[8];
+                        double complex A_neg_components[8];
+
+                        decompose_su3(A,A_components);
+                        decompose_su3(A_neg,A_neg_components);
+
+                        // Update running two-point sum
+                        for(int i=0; i<8; i++){
+                            D[counter] += A_components[i]*A_neg_components[i];
+                        }
+                        
+                    }// mu loop
+                    
+                    // Multiply by prefactors:
+                    
+                    if(qx+qy+qz < 1){
+                        D[counter] *= 2.0/((Nc*Nc-1)*Nd*Nt*Ns*Ns*Ns);
+                    }else{
+                        D[counter] *= 2.0/((Nc*Nc-1)*(Nd-1)*Nt*Ns*Ns*Ns);
+                    }                    
+                    
+                    counter += 1;
+                }//qz loop
+            }//qy loop    
+        }//qx loop
+    }//t loop
+    
+}//program
